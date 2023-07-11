ebb51a1e51a2476afc57b5693fddee7473df89ed
ImpoSher <139252130+ImpoSher@users.noreply.github.com>
2023-07-11 11:37:16 -0400

    ImpoSher world project
    a role play game based on my youtube channel called impoSher

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @ImpoSher i create one big project based after a channel on youtube i own. such as the project i work on is called ImpoSher world, its named and programed after my channel called ImpoSher.
+- 👀 I’m interested in ... among us, programing, five nights at freddies, animating 2 dimentional videos
+- 🌱 I’m currently learning ... teaching myself how to code games
+- 💞️ I’m looking to collaborate on ... nothing
+- 📫 How to reach me ... nothing
+
+<!---
+ImpoSher/ImpoSher is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
